55bad1cf5c51dd0e9010632cc3121ba0f2e430e3
Sammy Ouhbout <ouhboutsammy@gmail.com>
2021-04-20 11:36:41 +0200

    update

--- trainyourprogrammer/338_Turmspringen.py
+++ trainyourprogrammer/338_Turmspringen.py
@@ -3,17 +3,13 @@
 #    Liste1 = [float (x) for x in input("Bitte geben sie 7 Wertungen an:   ")]
 #    print (Liste1)
 
-def
 Liste1 = [7.5, 8, 10, 9, 5, 8.5, 2]
 Multiplikator = [2.1]
+Liste1.sort()
 print(Liste1)
 
-
-Liste1sorted = Liste1.sort()
-
-print(Liste1sorted)
-
-
+Listeedit = Liste1[-2::2]
+print(Listeedit)
 
 
 
