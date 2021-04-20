54a0122b483d63a0eb115bd5d173d4d95b503e46
Jannes Harbecke <jannes.harbecke@gmail.com>
2021-04-20 11:09:24 +0200

    classes Kasse and Filiale

---
+++ trainyourprogrammer/55_Firmenumsatz.py
@@ -0,0 +1,36 @@
+# Die Zentrale einer Supermarktkette möchte die Umsatzzahlen ermitteln.
+# Hierzu läßt sie sich die Zahlen aus den einzelnen Filialen geben.
+# Diese haben wiederum unterschiedliche Kassen, die morgens mit einer gewissen Anfangsumme bestückt werden und am Abend eine Endsumme haben.
+
+# Erstelle entsprechende Klassen mit sinnvollen Methoden um den Gesamtumsatz zu berechnen.
+
+
+import random
+
+class Kasse():
+    def __init__(self):
+        super().__init__()
+
+        self.Kassenstand = 250
+
+    def kassenstandänderung(self, val):
+        self.Kassenstand+=val
+
+    def diebstahl(self):
+        self.Kassenstand = 0
+
+
+class Filiale():
+    def __init__(self):
+        super().__init__()
+
+        self.Kassen = []
+        self.GesamtKassenstand = 0
+
+        for i in range(random.randint(4,10)):
+            self.Kassen.append(Kasse)
+
+    def GesamtKassenstandberechnen(self):
+        for i in range(len(self.Kassen)):
+            GesamtKassenstand += self.Kassen[i]
+
